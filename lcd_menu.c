2715b28630963ade19b13a5caa5b407c3187c0dc
Grazvidas <grazvidasb@gmail.com>
2013-09-29 21:01:17 +0300

    created LCD_Menu: Menu, Options structures

--- lcd_menu.c
+++ lcd_menu.c
@@ -5,7 +5,31 @@
  *      Author: Graþvidas
  */
 
+#include "lcd_menu.h"
+
+LCD_Menu_t LCD_Menu = {LCD_Menu_enter,
+					  {LCD_Menu_LED_color,
+					   LCD_Menu_Settings,
+					   LCD_Menu_Channels,
+					   LCD_Menu_Profiles}
+					  };
 
 void LCD_Menu_enter() {
+
+}
+
+void LCD_Menu_LED_color() {
+
+}
+
+void LCD_Menu_Settings() {
+
+}
+
+void LCD_Menu_Channels() {
+
+}
+
+void LCD_Menu_Profiles() {
 
 }
--- lcd_menu.h
+++ lcd_menu.h
@@ -8,8 +8,25 @@
 #ifndef LCD_MENIU_H_
 #define LCD_MENIU_H_
 
+void LCD_Menu_enter(void);
+void LCD_Menu_LED_color(void);
+void LCD_Menu_Settings(void);
+void LCD_Menu_Channels(void);
+void LCD_Menu_Profiles(void);
+
+typedef struct {
+	void (*LED_color)(void);
+	void (*Settings)(void);
+	void (*Channels)(void);
+	void (*Profiles)(void);
+}Options_t;
+
 typedef struct {
 	void (*Enter)(void);
-}LCD_Menu;
+
+	Options_t Options;
+}LCD_Menu_t;
+
+extern LCD_Menu_t LCD_Menu;
 
 #endif /* LCD_MENIU_H_ */
