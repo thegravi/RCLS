92050065bc3f6ab34ba3f42b8c85ca103869f2f1
Grazvidas <grazvidasb@gmail.com>
2013-09-30 17:52:50 +0300

    created individual structures of each menu option

--- lcd_menu.c
+++ lcd_menu.c
@@ -6,15 +6,29 @@
  */
 
 #include "lcd_menu.h"
+#include <avr/io.h>
 
 LCD_Menu_t LCD_Menu = {LCD_Menu_enter,
 					  {LCD_Menu_LED_color,
-					   LCD_Menu_Settings,
+					   LCD_Menu_Preferences,
 					   LCD_Menu_Channels,
 					   LCD_Menu_Profiles}
 					  };
 
 void LCD_Menu_enter() {
+
+	//LCD_Menu.Options
+
+	uint8_t option = 0;
+
+	switch(option)
+	{
+
+
+	default:
+		break;
+
+	}
 
 }
 
@@ -22,7 +36,7 @@ void LCD_Menu_LED_color() {
 
 }
 
-void LCD_Menu_Settings() {
+void LCD_Menu_Preferences() {
 
 }
 
--- lcd_menu.h
+++ lcd_menu.h
@@ -10,15 +10,41 @@
 
 void LCD_Menu_enter(void);
 void LCD_Menu_LED_color(void);
-void LCD_Menu_Settings(void);
+void LCD_Menu_Preferences(void);
 void LCD_Menu_Channels(void);
 void LCD_Menu_Profiles(void);
 
 typedef struct {
-	void (*LED_color)(void);
-	void (*Settings)(void);
-	void (*Channels)(void);
-	void (*Profiles)(void);
+	char* name;
+
+}LED_t;
+
+typedef struct {
+	char* name;
+
+}Settings_t;
+
+typedef struct {
+	char* name;
+
+}Channels_t;
+
+typedef struct {
+	char* name;
+
+}Profiles_t;
+
+typedef struct {
+	void (*Opt_LED_color)(void);
+	void (*Opt_Preferences)(void);
+	void (*Opt_Channels)(void);
+	void (*Opt_Profiles)(void);
+
+	LED_t LED;
+	Settings_t Prefs;
+	Channels_t Ch;
+	Profiles_t Profiles;
+
 }Options_t;
 
 typedef struct {
