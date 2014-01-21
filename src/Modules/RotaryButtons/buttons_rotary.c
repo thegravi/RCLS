962e018b0a5198ce91dcc40f17cd0e235d9c5b32
Grazvidas <grazvidasb@gmail.com>
2014-01-21 20:21:41 +0200

    added rotary buttons empty module

---
+++ src/Modules/RotaryButtons/buttons_rotary.c
@@ -0,0 +1,8 @@
+/*
+ * buttons_rotary.c
+ *
+ *  Created on: 2014 saus. 21
+ *      Author: Gražvidas
+ */
+
+#include "buttons_rotary.h"
---
+++ src/Modules/RotaryButtons/buttons_rotary.h
@@ -0,0 +1,13 @@
+/*
+ * buttons_rotary.h
+ *
+ *  Created on: 2014 saus. 21
+ *      Author: Gražvidas
+ */
+
+#ifndef BUTTONS_ROTARY_H_
+#define BUTTONS_ROTARY_H_
+
+
+
+#endif /* BUTTONS_ROTARY_H_ */
