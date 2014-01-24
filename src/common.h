5d2125c4d2ee53789af600537bd1e7b41e454548
Grazvidas <grazvidasb@gmail.com>
2014-01-24 18:16:31 +0200

    added enable/ disable defines

--- src/common.h
+++ src/common.h
@@ -8,11 +8,13 @@
 #ifndef COMMON_H_
 #define COMMON_H_
 
-#define TRUE	1
-#define FALSE	0
-#define SUCC	1
-#define FAIL	0
-#define ERR		-1
+#define TRUE		1
+#define FALSE		0
+#define SUCC		1
+#define FAIL		0
+#define ERR			-1
+#define ENABLE		TRUE
+#define DISABLE		FALSE
 
 #define LED_ON() 	DDRC |= 1<<PC5; PORTC |= 1<<PC5;
 #define LED_OFF() 	DDRC &= ~(1<<PC5); PORTC &= ~(1<<PC5);
