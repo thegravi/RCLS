8d9d2cb85dcd3f6c6a874ba1fdd1e6b2f319812e
Grazvidas <grazvidasb@gmail.com>
2014-01-25 12:45:21 +0200

    added PWM support

--- src/Drivers/PWM/pwm.c
+++ src/Drivers/PWM/pwm.c
@@ -10,13 +10,15 @@
 Pwm_t Pwm = {
 		PWM_Init,
 		PWM_Enable,
+		PWM_SetIntensity,
 		FALSE
 };
 
 void PWM_Init()
 {
 
-	// clock source for Red, Green channels
+//--------------- Red, Green channels --------------//
+	// clock source
 	TCCR0B = 1<<CS02;
 
 	// Fast PWM
@@ -25,14 +27,44 @@ void PWM_Init()
 	// non-inverting mode
 	TCCR0A |= 1<<COM0A1 | 1<<COM0B1;
 
+	// default 0
+	OCR0A = 0;OCR0B = 0;
+
+//--------------- Blue channels --------------//
+	// clock source
+	TCCR2B = 1<<CS22;
+
+	// Fast PWM
+	TCCR2A = 1<<WGM21 | 1<<WGM20;
+
+	// non-inverting mode
+	TCCR2A |= 1<<COM2B1;
+
+	// default zero
+	OCR2B = 0;
+
+//--------------- Brightness channel --------------//
+
+
+	LED_RGB_DDR |= 1<<LED_R_PIN | 1<<LED_B_PIN | 1<<LED_G_PIN;
 	Pwm.initSUCC = SUCC;
 }
 
 void PWM_Enable(uint8_t state)
 {
+	if (!Pwm.initSUCC)
+		return;
+
 	if (state == ENABLE)
 	{
+		// For Red, Green channels
+		TCCR0B |= 1<<CS02;
+		TCCR0A |= 1<<COM0B1 | 1<<COM0A1 | 1<<WGM01 | 1<<WGM00;
 
+		// For Blue channel
+		TCCR2B |= 1<<CS02;
+		TCCR2A |= 1<<COM2B1 | 1<<WGM21 | 1<<WGM20;
+		//For Brightness channel
 	}
 	else
 	{
@@ -41,15 +73,16 @@ void PWM_Enable(uint8_t state)
 		TCCR0A &= ~(1<<COM0B1) & ~(1<<COM0A1) & ~(1<<WGM01) & ~(1<<WGM00);
 
 		// For Blue channel
-
+		TCCR2B &= ~(1<<CS02);
+		TCCR2A &= ~(1<<COM2B1) & ~(1<<WGM21) & ~(1<<WGM20);
 		//For Brightness channel
 
 	}
 }
 
-void PWM_SetIntensity(uint8_t color, uint8_t value)
+void PWM_SetIntensity(uint8_t obj, uint8_t value)
 {
-	switch(color)
+	switch(obj)
 	{
 	case Red:
 		OCR0A = value;
@@ -61,6 +94,9 @@ void PWM_SetIntensity(uint8_t color, uint8_t value)
 
 	case Blue:
 		OCR2B = value;
+		break;
+
+	case Brightness:
 		break;
 
 	default:
--- src/Drivers/PWM/pwm.h
+++ src/Drivers/PWM/pwm.h
@@ -14,24 +14,28 @@
 #define LED_R_PIN			PD6
 #define LED_G_PIN			PD5
 #define LED_B_PIN			PD3
-#define LED_RGB_PORT		DDRD
+#define LED_RGB_DDR			DDRD
 
 #define LED_BRIGHT_PIN		PB1
-#define LED_BRIGHT_PORT		DDRB
+#define LED_BRIGHT_DDR		DDRB
 
 void PWM_Init(void);
+void PWM_Enable(uint8_t state);
+void PWM_SetIntensity(uint8_t obj, uint8_t value);
 
 typedef enum {
 	Red=0,
 	Green,
 	Blue,
+	Brightness,
 
-	NumOfColors
-}Colors_t;
+	States
+}LED_State_t;
 
 typedef struct {
 	void (*Init)(void);
 	void (*Enable)(uint8_t state);
+	void (*SetIntensity)(uint8_t obj, uint8_t value);
 
 	uint8_t initSUCC;
 
