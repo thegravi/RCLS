58d00f7ec15bb336af255ca2662eaa23affe264d
Gražvidas Balčius <grazvidas@elektromotus.lt>
2014-01-26 00:54:00 +0200

    included ADC/ PWM libraries

--- src/main.c
+++ src/main.c
@@ -7,10 +7,12 @@
 
 #include <avr/io.h>
 #include "Drivers/LCD/lcd.h"
+#include "Drivers/UART/uart.h"
+#include "Drivers/ADC/ADC.h"
+#include "Drivers/PWM/pwm.h"
 #include "Modules/Menu/lcd_menu.h"
 #include "Modules/PushButtons/buttons_push.h"
 #include "Modules/RotaryButtons/buttons_rotary.h"
-#include "Drivers/UART/uart.h"
 #include "common.h"
 
 #define F_CPU 8000000UL
@@ -18,12 +20,26 @@
 void Init(void);
 
 int8_t status = -1;
-
+int i = 0, j = 0;
 int main()
 {
 	DDRC |= 1<<PC5;
 
 	Init();
+
+//	while(1)
+//	{
+//		for (i=0;i<256;i++)
+//		{
+//			Pwm.SetIntensity(j, i);
+//			_delay_ms(50);
+//		}
+//
+//		if (j==2)
+//			j=0;
+//		else
+//			j++;
+//	}
 
 	while(1)
 	{
@@ -42,6 +58,7 @@ void Init()
 	cli();
 //	UART.Init();
 	LCD.Init();
+//	Pwm.Init();
 	Menu.Init();
 	Buttons.Init();
 	sei();
