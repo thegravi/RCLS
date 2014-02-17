ee14b77930ee253ccc52a635219eb5a6ac96f640
Grazvidas <grazvidasb@gmail.com>
2014-02-17 17:47:52 +0200

    implemented Init func, added channel defines for rotary buttons

--- Modules/RotaryButtons/buttons_rotary.c
+++ Modules/RotaryButtons/buttons_rotary.c
@@ -6,3 +6,13 @@
  */
 
 #include "buttons_rotary.h"
+
+Rotary_Interface_t Rotary = {
+		ROTARY_Init
+};
+
+void ROTARY_Init()
+{
+	DDR_ROT &= ~(1<<ROT_CH_R) & ~(1<<ROT_CH_G) & ~(1<<ROT_CH_B) & ~(1<<ROT_CH_BRIGH);
+	Adc.Init();
+}
--- Modules/RotaryButtons/buttons_rotary.h
+++ Modules/RotaryButtons/buttons_rotary.h
@@ -8,6 +8,21 @@
 #ifndef BUTTONS_ROTARY_H_
 #define BUTTONS_ROTARY_H_
 
+#include "../../Drivers/ADC/ADC.h"
 
+#define DDR_ROT		 	DDRC
+#define ROT_CH_R	 	PC5
+#define ROT_CH_G	 	PC4
+#define ROT_CH_B	 	PC5
+#define ROT_CH_BRIGH	PC5
+
+typedef struct {
+	void (*Init)(void);
+
+}Rotary_Interface_t;
+
+extern Rotary_Interface_t Rotary;
+
+void ROTARY_Init(void);
 
 #endif /* BUTTONS_ROTARY_H_ */
